ee5c761f6129d65518819476abec2e714a4a21db
YendisFish <brintonsid@gmail.com>
2024-01-10 17:23:24 -0700

    Init Commit

---
+++ Components/StateComponent.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace EasyBlazor.Components;
+
+public class StateComponent : ComponentBase
+{
+    [Parameter] public string Class { get; set; } = "";
+    [Parameter] public string Id { get; set; } = "";
+
+    public void Update() => StateHasChanged();
+}
---
+++ Components/Tables/EasyHeader.razor.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace EasyBlazor.Components.Tables;
+
+public partial class EasyHeader : StateComponent
+{
+    [Parameter] public List<string> Columns { get; set; } = new();
+    [Parameter] public string ColumnClass { get; set; } = "";
+    [Parameter] public string ColumnId { get; set; } = "";
+}
