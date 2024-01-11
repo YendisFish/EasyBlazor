efc036cf135847d4b15ddf3cc5ca9e42f46802e7
YendisFish <brintonsid@gmail.com>
2024-01-11 10:40:29 -0700

    added second Interop option

--- Interop.cs
+++ Interop.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.JSInterop;
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
 
 namespace EasyBlazor;
 
 public static class Interop
 {
     public static void SetById(string id, string html, IJSRuntime runtime) => runtime.InvokeVoidAsync("setById", CancellationToken.None, new object[] { id, html });
+    public static void SetById(string id, MarkupString html, IJSRuntime runtime) => runtime.InvokeVoidAsync("setById", CancellationToken.None, new object[] { id, html.Value });
 }
